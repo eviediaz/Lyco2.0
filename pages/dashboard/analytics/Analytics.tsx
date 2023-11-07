717f17c716c13dec0de957998f66c43b2746caaa
eviediaz <evieediaz@icloud.com>
2023-11-07 05:44:57 +0000

    commit all

---
+++ pages/dashboard/analytics/Analytics.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const analytics = () => {
+    return (
+        <h1>analytics</h1>
+    )
+}
+
+export default analytics;
---
+++ pages/login/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const login = () => {
+    return (
+        <h1>Login test</h1>
+    )
+}
+
+export default login;
